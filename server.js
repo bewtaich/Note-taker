8a6456e15787a2db60e7524f9e13fcf0a541b664
Bewtaich <bewtaich@gmail.com>
2024-08-29 20:53:12 -0500

    Post route running

--- server.js
+++ server.js
@@ -1,22 +1,57 @@
-
-const express = require('express');
-const path = require('path')
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes', (req, res) =>{
-    res.sendFile(path.join(__dirname, 'public' ,'notes.html'))
-  });
-
-app.get('*', (req, res) => {
-    res.status(404).send('Page Not Found');
+app.get("/api/notes", (req, res) => {
+  const notes = require("./db/db.json");
+  res.json(notes);
 });
-  
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "notes.html"));
+});
+
+app.post("/api/notes", (req, res) => {
+  fs.readFile(
+    path.join(__dirname, "db", "db.json"),
+    "utf-8",
+    (err, jsonData) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      let notes = JSON.parse(jsonData);
+      const newNote = req.body;
+      notes.push(newNote);
+
+      fs.writeFile(
+        path.join(__dirname, "db", "db.json"),
+        JSON.stringify(notes),
+        (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          res.json(newNote)
+        }
+      );
+    }
+  );
+});
+
+app.delete();
+
+app.get("*", (req, res) => {
+  res.status(404).send("Page Not Found");
+});
+
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}`);
+  console.log(`Listening on ${PORT}`);
 });
