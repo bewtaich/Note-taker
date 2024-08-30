3f904393b108d7f1e129a174ea120a33a2fc6dd8
Bewtaich <bewtaich@gmail.com>
2024-08-29 21:25:15 -0500

    created delete route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -28,7 +29,7 @@ app.post("/api/notes", (req, res) => {
       }
 
       let notes = JSON.parse(jsonData);
-      const newNote = req.body;
+      const newNote = { ...req.body, id: uuidv4() };
       notes.push(newNote);
 
       fs.writeFile(
@@ -39,14 +40,45 @@ app.post("/api/notes", (req, res) => {
             console.error(err);
             return;
           }
-          res.json(newNote)
+          res.json(newNote);
         }
       );
     }
   );
 });
 
-app.delete();
+app.delete("/api/notes/:id", (req, res) => {
+  const noteId = req.params.id;
+  console.log(noteId);
+
+  fs.readFile(
+    path.join(__dirname, "db", "db.json"),
+    "utf-8",
+    (err, jsonData) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+    let notes = JSON.parse(jsonData);
+    notes = notes.filter(note => note.id === noteId) 
+
+    fs.writeFile(
+        path.join(__dirname, "db", "db.json"),
+        JSON.stringify(notes),
+        (err) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          res.json(noteId);
+        }
+      );
+    }
+
+    
+  );
+});
 
 app.get("*", (req, res) => {
   res.status(404).send("Page Not Found");
